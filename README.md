f27c5f119a3d44993ac9418930166b57452eea0e
blackwhite2018 <39052924+blackwhite2018@users.noreply.github.com>
2020-06-03 14:46:41 +0300

    initial repo, add func component

---
+++ README.md
@@ -0,0 +1,16 @@
+Shoping card component (function based)
+
+## Installation
+
+`npm install` for installed dependencies
+
+## Available Scripts
+
+### `npm start`
+
+Start devServer for development
+
+### `npm run build`
+
+Project build
+
---
+++ src/ShopItemFunc.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ShopItemFunc = ({ item: { brand, title, description, descriptionFull, price, currency } }) => (
+    <div className="main-content">
+        <h2>{ brand }</h2>
+        <h1>{ title }</h1>
+        <h3>{ description }</h3>
+        <div className="description">{ descriptionFull }</div>
+        <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+            <div className="price">{ currency + price.toFixed(2) }</div>
+            <button>Добавить в корзину</button>
+        </div>
+    </div>
+);
+
+ShopItemFunc.propTypes = {
+    item: PropTypes.object,
+    brand: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    descriptionFull: PropTypes.string,
+    price: PropTypes.number,
+    currency: PropTypes.string
+};
+
+export default ShopItemFunc;
+
